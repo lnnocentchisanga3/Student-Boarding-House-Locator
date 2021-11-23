2d18df9288f71a42a46d01678728494d830e4a71
zikana <56475756+zikana@users.noreply.github.com>
2021-11-23 02:49:06 -0800

    std & ldld

--- auth-login.php
+++ auth-login.php
@@ -83,34 +83,13 @@ if (isset($_GET['email'])) {
                       please fill in your password
                     </div>
                   </div>
-                  <!-- <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
-                      <label class="custom-control-label" for="remember-me">Remember Me</label>
-                    </div>
-                  </div> -->
+                 
                   <div class="form-group">
                     <button type="submit" name="login" class="btn btn-primary btn-lg btn-block" tabindex="4">
                       Login
                     </button>
                   </div>
                 </form>
-                <!-- <div class="text-center mt-4 mb-3">
-                  <div class="text-job text-muted">Login With Social</div>
-                </div>
-                <div class="row sm-gutters">
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                      <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                  </div>
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-google">
-                      <span class="fab fa-google"></span> google
-                    </a>
-                  </div>
-                </div> -->
-
                 <div class="mt-5 text-muted text-center">
                   Don't have an account? <a href="login.php">Create One</a>
                 </div>
